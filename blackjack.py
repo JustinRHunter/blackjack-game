23dc20fd5698ec0e5a93400959f471c302306c98
Justin Hunter <jhun2452@gmail.com>
2022-10-07 14:51:19 +1100

    first commit

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,12 @@
-blackjack game
+class Player():
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return ("Hello, {name}".format(name = self.name))
+
+justin_hunter = Player("Justin")
+
+## Welcome message
+print("Welcome to Triads Casino, where only the most villanous can play. Many have tried to beat the Triads but few have succeeded... Do you have what it takes?")
+print("Test")
