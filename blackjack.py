072efd011056074785bbfa0d13bff726a340d408
Justin Hunter <jhun2452@gmail.com>
2022-10-07 14:59:34 +1100

    getting user name

--- blackjack.py
+++ blackjack.py
@@ -6,5 +6,11 @@ class Player():
         return ("Hello, {name}".format(name = self.name))
 
 
-justin_hunter = Player("Justin")
+## Welcome message
+print("Welcome to Triads Casino, where only the most villanous can play. Many have tried to beat the Triads but few have succeeded... Do you have what it takes?")
+print("Perhaps it is too early to tell but time reveals all..")
+new_player = Player(input("Why don't you tell me your name young buck and we can get started?"))
+print(new_player)
+
+
 
