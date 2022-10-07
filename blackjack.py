72c73e5b3afa873cd5e792a5efa83c850b1ea20f
Justin Hunter <jhun2452@gmail.com>
2022-10-07 14:52:53 +1100

    why won't instance print name?

--- blackjack.py
+++ blackjack.py
@@ -5,8 +5,6 @@ class Player():
     def __repr__(self):
         return ("Hello, {name}".format(name = self.name))
 
+
 justin_hunter = Player("Justin")
 
-## Welcome message
-print("Welcome to Triads Casino, where only the most villanous can play. Many have tried to beat the Triads but few have succeeded... Do you have what it takes?")
-print("Test")
